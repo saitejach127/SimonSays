37bb7990c71c9d6cfdcb4a571cfddb32289ba8ce
saitejach127 <saitejach127@gmail.com>
2020-11-21 11:42:40 +0530

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import './bootstrap.min.css';
 
 function App() {
   return (
