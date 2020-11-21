ed10fc466e1630f66e45fbed622ae79e741407ef
saitejach127 <saitejach127@gmail.com>
2020-11-21 12:41:00 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Task from './components/simonsays/Task';
+import Score from './components/simonsays/Score';
 import './bootstrap.min.css';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/simonsays/task" component={Task} />
-        <Route exact path="/simonsays/score" component={Task} />
+        <Route exact path="/simonsays/score" component={Score} />
       </Router>
     </React.Fragment>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Home() {
     return (
         <div>
             <h1>Home Page</h1>
+            <Link to="/simonsays/task" className="btn btn-primary">Simon Says</Link>
         </div>
     )
 }
