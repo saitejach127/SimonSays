4bca79d4fdf6c10a6daa29aef81c813a29c87b5f
siddhant <siddhant.chawla9@gmail.com>
2020-11-21 17:19:49 +0530

    Leaderboard Created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Simon from './components/simonsays/Main'
 import Task from './components/simonsays/Task';
 import Score from './components/simonsays/Score';
+import Scorecard from './components/simonsays/Scorecard';
 import './bootstrap.min.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/simonsays" component={Simon} />
         <Route exact path="/simonsays/task" component={Task} />
         <Route exact path="/simonsays/score" component={Score} />
+        <Route exact path="/simonsays/leaderboard" component={Scorecard} />
       </Router>
     </React.Fragment>
   );
---
+++ src/components/simonsays/Scorecard.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import Navbar from '../Navbar';
+import Footer from '../Footer';
+
+
+function createData(name, score) {
+    return { name, score };
+  }
+
+
+export default function Main() {
+
+    var rows = [
+        createData("Siddhant", 25),
+        createData("Sai Teja", 22),
+        createData("Anshika", 30),
+    ]
+
+    var name = "Siddhant"
+
+    rows.sort(function (a,b)
+    {
+        return b.score - a.score
+    })
+
+    return (
+        <>
+            <Navbar />
+            <div><br></br></div>
+            <div><br></br></div>
+            <div className = "container">
+                <center><h2>LeaderBoard</h2></center>
+            </div>
+            <div><br></br></div>
+            <div className="container">
+                <center>
+                <table class="table table-hover">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows.map((row,index) => (
+                    <tr class={row.name == name?"table-primary":""}>
+                    <th scope="row">{index+1}</th>
+                    <td>{row.name}</td>
+                    <td>{row.score}</td>
+                    </tr>
+                ))}
+                    
+                </tbody>
+                </table>
+                </center>
+            </div>
+            <div><br></br></div>
+            <div><br></br></div>
+            <Footer />
+        </>
+    )
+}
